8ee69d20554d556e503d6eabea1798cc13994fdb
Ben John Bagley <ben@benbagley.co.uk>
2018-06-23 16:04:10 +0100

    homepage done

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,10 +2,14 @@ $(document).ready(function(){
   $(window).scroll(function(){
   	var scroll = $(window).scrollTop();
 	  if (scroll > 300) {
-	    $(".navbar").css("background" , "rgba(90, 46, 214, .80)");
+	    $(".navbar").css("background" , "#5a2ed6");
 	  }
 	  else{
 		  $(".navbar").css("background" , "transparent");  	
 	  }
-  })
+	})
+
+	$('.navbar-toggler').click(function(){
+		$(this).find('i').toggleClass('fa-times text-white');
+	});
 })
