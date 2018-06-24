2916b09c6ffc661eb146c5ca1c14a4c5a0c2430a
Ben John Bagley <ben@benbagley.co.uk>
2018-06-24 18:00:21 +0100

    static done

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,25 @@
-$(document).ready(function(){
-  $(window).scroll(function(){
+$(document).ready(function() {
+  $(window).scroll(function() {
   	var scroll = $(window).scrollTop();
 	  if (scroll > 300) {
 	    $(".navbar").css("background" , "#5a2ed6");
 	  }
-	  else{
+	  else {
 		  $(".navbar").css("background" , "transparent");  	
 	  }
 	})
 
-	$('.navbar-toggler').click(function(){
+	$('.navbar-toggler').click(function() {
 		$(this).find('i').toggleClass('fa-times text-white');
-	});
+	})
+
+	$('#what-we-do a.who-are-do_grid').click(function() {
+		$(this).find('.more-info').css('display', 'block');
+		return false;
+	})
+
+	$('.more-info').click(function() {
+		$('.more-info').css('display', 'none');
+		return false;
+	})
 })
